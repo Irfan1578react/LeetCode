25b747af35320bc60edbbd3f36f47b64f82f3f77
Irfan1578react <164026943+Irfan1578react@users.noreply.github.com>
2024-05-06 22:45:10 +0530

    Time: 6 ms (11.62%), Space: 42.1 MB (30.1%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int max=-1;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(i<j && nums[i] <nums[j]){
+                    if(max<nums[j]-nums[i]){
+                        max=nums[j]-nums[i];
+                    }
+                }
+            }
+        }
+        return max;
+        
+    }
+}
