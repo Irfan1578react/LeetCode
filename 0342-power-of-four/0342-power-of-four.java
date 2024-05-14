269587b97bc2e044b14edafc59d667253ca06f37
Irfan1578react <164026943+Irfan1578react@users.noreply.github.com>
2024-05-14 18:30:23 +0530

    Time: 0 ms (100%), Space: 40.6 MB (56.14%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        while(n!=4){
+            
+            if(n%4!=0){
+                return false;
+            }
+            else{
+                n=n/4;
+            }
+        }
+        if(n==4 ){
+            return true;
+        }
+        return false;
+        
+    }
+}
